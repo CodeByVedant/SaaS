1a81e63d65dd7089fd3c8c29db2111f32836af3e
CodeByVedant <vedantsawant1128@gmail.com>
2026-09-20 17:48:42 +0530

    complete step 4 testing

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,41 +1,62 @@
-const ApiError = require("../utils/ApiError");
+const Joi = require("joi");
 
 const validate = (schema) => {
     return async (req, res, next) => {
         try {
-            const data = {
-                body: req.body,
-                params: req.params,
-                query: req.query
-            };
+            // Validate request params
+            if (schema.params) {
+                const validatedParams =
+                    await schema.params.validateAsync(
+                        req.params,
+                        {
+                            abortEarly: false,
+                            stripUnknown: true
+                        }
+                    );
 
-            const validated = await schema.validateAsync(
-                data,
-                {
-                    abortEarly: false,
-                    stripUnknown: true
-                }
-            );
+                req.params = validatedParams;
+            }
 
-            req.body = validated.body;
-            req.params = validated.params;
-            req.query = validated.query;
+            // Validate request query
+            if (schema.query) {
+                const validatedQuery =
+                    await schema.query.validateAsync(
+                        req.query,
+                        {
+                            abortEarly: false,
+                            stripUnknown: true
+                        }
+                    );
+
+                req.query = validatedQuery;
+            }
+
+            // Validate request body
+            if (schema.body) {
+                const validatedBody =
+                    await schema.body.validateAsync(
+                        req.body,
+                        {
+                            abortEarly: false,
+                            stripUnknown: true
+                        }
+                    );
+
+                req.body = validatedBody;
+            }
 
             next();
 
         } catch (error) {
-
-            if (error.isJoi) {
-                return next(
-                    new ApiError(
-                        400,
-                        "Validation failed",
-                        error.details.map((detail) => ({
-                            field: detail.path.join("."),
-                            message: detail.message
-                        }))
-                    )
-                );
+            if (error instanceof Joi.ValidationError) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Validation failed",
+                    errors: error.details.map((detail) => ({
+                        field: detail.path.join("."),
+                        message: detail.message
+                    }))
+                });
             }
 
             next(error);
@@ -44,3 +65,57 @@ const validate = (schema) => {
 };
 
 module.exports = validate;
+
+
+
+
+
+
+
+
+// const ApiError = require("../utils/ApiError");
+
+// const validate = (schema) => {
+//     return async (req, res, next) => {
+//         try {
+//             const data = {
+//                 body: req.body,
+//                 params: req.params,
+//                 query: req.query
+//             };
+
+//             const validated = await schema.validateAsync(
+//                 data,
+//                 {
+//                     abortEarly: false,
+//                     stripUnknown: true
+//                 }
+//             );
+
+//             req.body = validated.body;
+//             req.params = validated.params;
+//             req.query = validated.query;
+
+//             next();
+
+//         } catch (error) {
+
+//             if (error.isJoi) {
+//                 return next(
+//                     new ApiError(
+//                         400,
+//                         "Validation failed",
+//                         error.details.map((detail) => ({
+//                             field: detail.path.join("."),
+//                             message: detail.message
+//                         }))
+//                     )
+//                 );
+//             }
+
+//             next(error);
+//         }
+//     };
+// };
+
+// module.exports = validate;
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,8 +1,7 @@
 const express =
     require("express");
 
-const router =
-    express.Router();
+const router = express.Router({ mergeParams: true });
 
 
 const protect =
